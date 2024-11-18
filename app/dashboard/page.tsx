cb102dc7b43b331bcdd0a20821f4988d3169a60b
reywijaya <renaldywijayaputra@gmail.com>
2024-11-18 12:58:04 +0700

    feat: fetch latest invoices

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,10 +1,13 @@
 import {lusitana} from "@/app/ui/fonts";
-import {fetchRevenue} from "@/app/lib/data";
+import {fetchLatestInvoices, fetchRevenue} from "@/app/lib/data";
 import RevenueChart from "@/app/ui/dashboard/revenue-chart";
+import LatestInvoices from "@/app/ui/dashboard/latest-invoices";
 
 export default async function Page() {
 
     const revenue = await fetchRevenue()
+    const latestInvoices = await fetchLatestInvoices()
+
     return (
         <main>
             <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>Dashboard</h1>
@@ -20,7 +23,7 @@ export default async function Page() {
             </div>
             <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
                 <RevenueChart revenue={revenue}  />
-                {/* <LatestInvoices latestInvoices={latestInvoices} /> */}
+                <LatestInvoices latestInvoices={latestInvoices} />
             </div>
         </main>
     )
