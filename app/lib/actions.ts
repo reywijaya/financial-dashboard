082d27d2ddc25527a3e4d8654f72c6ac86748eb0
reywijaya <renaldywijayaputra@gmail.com>
2024-11-28 16:50:32 +0700

    fix eslint errors

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -53,6 +53,7 @@ export async function createInvoice(prevState: State, formData: FormData) {
             VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
         `;
     } catch (error) {
+        console.error(error);
         return {
             message: "Database Error, Failed to Create Invoice",
         }
@@ -95,6 +96,7 @@ export async function updateInvoice(
             WHERE id = ${id}
         `;
     } catch (error) {
+        console.error(error);
         return {
             message: "Database Error, Failed to Update Invoice",
         }
@@ -112,6 +114,7 @@ export async function deleteInvoice(id: string) {
         revalidatePath('/dashboard/invoices');
         return {message: "Deleted Invoice"};
     } catch (error) {
+        console.error(error);
         return {
             message: "Database Error, Failed to Delete Invoice",
         }
